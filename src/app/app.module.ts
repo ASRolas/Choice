8875c0a1a488010ad176e28a32991a4e48eca4a2
Germano Kazembe <johnyjerry53@gmail.com>
2021-12-01 16:29:28 +0200

    the start of a journey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AutenticacaoComponent } from './components/autenticacao/autenticacao.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { exportacoes } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
+    exportacoes,
     AppComponent,
     HomeComponent,
     AutenticacaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatIconModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
