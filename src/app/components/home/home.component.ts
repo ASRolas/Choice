2523218f1f160a4936fb5de07ee9895985afb0e3
Germano Kazembe <johnyjerry53@gmail.com>
2021-12-05 16:51:48 +0200

    adicionei efeitos ao passar o mouse nas opcoes da div esquerda da home page e na marca dágua na terceira barra (class=barra_links)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -19,4 +19,66 @@ export class HomeComponent implements OnInit {
 
   }
 
+  pairar(){
+    
+
+    $("#bl1").mouseover(function(){
+      $("#bl1").css("background","rgb(156, 26, 17)"); 
+      $("#bl1").css("color","white");
+    });
+    $("#bl1").mouseout(function(){
+      $("#bl1").css("background","rgb(238, 228, 228)"); 
+      $("#bl1").css("color","gray");
+    });
+
+    $("#bl2").mouseover(function(){
+      $("#bl2").css("background","rgb(156, 26, 17)"); 
+      $("#bl2").css("color","white");
+    });
+    $("#bl2").mouseout(function(){
+      $("#bl2").css("background","rgb(238, 228, 228)"); 
+      $("#bl2").css("color","gray");
+    });
+
+    //--------------------------
+    $("#bl3").mouseover(function(){
+      $("#bl3").css("background","rgb(156, 26, 17)"); 
+      $("#bl3").css("color","white");
+    });
+    $("#bl3").mouseout(function(){
+      $("#bl3").css("background","rgb(238, 228, 228)"); 
+      $("#bl3").css("color","gray");
+    });
+
+    
+    //--------------------------
+    $("#bl4").mouseover(function(){
+      $("#bl4").css("background","rgb(156, 26, 17)"); 
+      $("#bl4").css("color","white");
+    });
+    $("#bl4").mouseout(function(){
+      $("#bl4").css("background","rgb(238, 228, 228)"); 
+      $("#bl4").css("color","gray");
+    });
+
+     
+    //--------------------------
+    $("#watermark").mouseover(function(){
+      
+      $("#watermark").css("color","rgb(207, 209, 194)");
+    });
+    $("#watermark").mouseout(function(){
+     
+      $("#watermark").css("color","rgb(243, 234, 234)");
+    });
+
+    //--------------------------
+    
+    
+
+  }
+  
+;
+  
+
 }
