46864e3fc9a50aaf69c51b4418511d824af08994
Germano Kazembe <johnyjerry53@gmail.com>
2021-12-06 11:28:31 +0200

    Meu puto! arranjem-me uma faculdade para eu dar aulas de Angular..ntlha!

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,158 +7,145 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  imagem=`<img src="assets/comida1.jpg" style="height:100px; width:200px">`
-  constructor() { }
-  
-
-  ngOnInit(): void {
-  }
-
-  cor(){
-
-  
-    $(".lateral").css("background","white");
-
-  }
-
-  pairar(){
-    
-
-    $("#bl1").mouseover(function(){
-      $("#bl1").css("background","rgb(156, 26, 17)"); 
-      $("#bl1").css("color","white");
-    });
-    $("#bl1").mouseout(function(){
-      $("#bl1").css("background","rgb(238, 228, 228)"); 
-      $("#bl1").css("color","gray");
-    });
-
-    $("#bl2").mouseover(function(){
-      $("#bl2").css("background","rgb(156, 26, 17)"); 
-      $("#bl2").css("color","white");
-    });
-    $("#bl2").mouseout(function(){
-      $("#bl2").css("background","rgb(238, 228, 228)"); 
-      $("#bl2").css("color","gray");
-    });
-
-    //--------------------------
-    $("#bl3").mouseover(function(){
-      $("#bl3").css("background","rgb(156, 26, 17)"); 
-      $("#bl3").css("color","white");
-    });
-    $("#bl3").mouseout(function(){
-      $("#bl3").css("background","rgb(238, 228, 228)"); 
-      $("#bl3").css("color","gray");
-    });
-
-    
-    //--------------------------
-    $("#bl4").mouseover(function(){
-      $("#bl4").css("background","rgb(156, 26, 17)"); 
-      $("#bl4").css("color","white");
-    });
-    $("#bl4").mouseout(function(){
-      $("#bl4").css("background","rgb(238, 228, 228)"); 
-      $("#bl4").css("color","gray");
-    });
-
-     //--------------------------
-     $("#bl5").mouseover(function(){
-      $("#bl5").css("background","rgb(156, 26, 17)"); 
-      $("#bl5").css("color","white");
-    });
-    $("#bl5").mouseout(function(){
-      $("#bl5").css("background","rgb(238, 228, 228)"); 
-      $("#bl5").css("color","gray");
-    });
-
-     
-    //--------------------------
-    $("#watermark").mouseover(function(){
+      imagem=`<img src="assets/comida1.jpg" style="height:100px; width:200px">`
+      constructor() { }
       
-      $("#watermark").css("color","rgb(207, 209, 194)");
-    });
-    $("#watermark").mouseout(function(){
-     
-      $("#watermark").css("color","rgb(243, 234, 234)");
-    });
 
-    //--------------------------
-    
-    
-
-  }
-
-  mostrarPratosAcessiveis(){
-     
-    
-
-    
-    $("#bl5").mouseover(function(){
-
-      $(".pratos_acessiveis").show();
-    });
-    $("#bl5").mouseout(function(){
-      $(".pratos_acessiveis").hide();
-    });
-
-
-  }
-
-  mostrarPratosNacionais(){
-     
-    
-
-    
-    $("#bl1").mouseover(function(){
-
-      $(".pratos_nacionais").show();
-    });
-    $("#bl1").mouseout(function(){
-      $(".pratos_nacionais").hide();
-    });
-
-    $(".pratos_nacionais").hide();
-
-
-
-
-  }
- 
-
-  
-  slide1(){
-
-     let contador=6;
-
-      function delay(ms: number) {
-        return new Promise( resolve => setTimeout(resolve, ms) );
+      ngOnInit(): void {
       }
 
-    
+      cor(){
 
-      $("#1").fadeIn("slow");
-      delay(3000)
-      $("#1").fadeOut("slow");
-      delay(6000)
-      $("#2").fadeIn("slow")
-      delay(3000)
-      $("#2").fadeOut("slow");
-      delay(3000)
-      $("#3").fadeIn("slow")
-      delay(3000)
-      $("#3").fadeOut("slow");
-      delay(3000)
+      
+        $(".lateral").css("background","white");
+
+      }
+
+      pairar(){
+
+        $("#bl1").mouseover(function(){
+          $("#bl1").css("background","rgb(156, 26, 17)"); 
+          $("#bl1").css("color","white");
+        });
+        $("#bl1").mouseout(function(){
+          $("#bl1").css("background","rgb(238, 228, 228)"); 
+          $("#bl1").css("color","gray");
+        });
+
+        $("#bl2").mouseover(function(){
+          $("#bl2").css("background","rgb(156, 26, 17)"); 
+          $("#bl2").css("color","white");
+        });
+        $("#bl2").mouseout(function(){
+          $("#bl2").css("background","rgb(238, 228, 228)"); 
+          $("#bl2").css("color","gray");
+        });
+
+        //--------------------------
+        $("#bl3").mouseover(function(){
+          $("#bl3").css("background","rgb(156, 26, 17)"); 
+          $("#bl3").css("color","white");
+        });
+        $("#bl3").mouseout(function(){
+          $("#bl3").css("background","rgb(238, 228, 228)"); 
+          $("#bl3").css("color","gray");
+        });
+
+        //--------------------------
+        $("#bl4").mouseover(function(){
+          $("#bl4").css("background","rgb(156, 26, 17)"); 
+          $("#bl4").css("color","white");
+        });
+        $("#bl4").mouseout(function(){
+          $("#bl4").css("background","rgb(238, 228, 228)"); 
+          $("#bl4").css("color","gray");
+        });
+
+        //--------------------------
+        $("#bl5").mouseover(function(){
+          $("#bl5").css("background","rgb(156, 26, 17)"); 
+          $("#bl5").css("color","white");
+        });
+        $("#bl5").mouseout(function(){
+          $("#bl5").css("background","rgb(238, 228, 228)"); 
+          $("#bl5").css("color","gray");
+        });
+
+        
+        //--------------------------efeito da marca d'agua----------------------------------
+        $("#watermark").mouseover(function(){
+          
+          $("#watermark").css("color","rgb(207, 209, 194)");
+        });
+        $("#watermark").mouseout(function(){
+        
+          $("#watermark").css("color","rgb(243, 234, 234)");
+        });
+
+        //--------------------------efeito do botao de itens salvos--------------------------------
+        
+        $("#salvos").mouseover(function(){
+          
+          $("#salvos").css("color","rgb(55, 83, 184)");
+        });
+        $("#salvos").mouseout(function(){
+        
+          $("#salvos").css("color","rgb(150, 7, 7)");
+        });
+
+        
+
+      }
+
+      mostrarPratosAcessiveis(){
+        
+        $("#bl5").mouseover(function(){
+          $(".pratos_acessiveis").show();
+        });
+
+        $("#bl5").mouseout(function(){
+          $(".pratos_acessiveis").hide();
+        });
+      }
+
+      mostrarPratosNacionais(){
+        
+        $("#bl1").mouseover(function(){
+
+          $(".pratos_nacionais").show();
+        });
+
+        $("#bl1").mouseout(function(){
+          $(".pratos_nacionais").hide();
+        });
+
+        $(".pratos_nacionais").hide();
+      }
+    
+      slide1(){
+
+        let contador=6;
+
+          function delay(ms: number) {
+            return new Promise( resolve => setTimeout(resolve, ms) );
+          }
+
+          $("#1").fadeIn("slow");
+          delay(3000)
+          $("#1").fadeOut("slow");
+          delay(6000)
+          $("#2").fadeIn("slow")
+          delay(3000)
+          $("#2").fadeOut("slow");
+          delay(3000)
+          $("#3").fadeIn("slow")
+          delay(3000)
+          $("#3").fadeOut("slow");
+          delay(3000)
+          
+      }
       
 
-
-   
-
-
-  }
-  
-;
   
 
 }
